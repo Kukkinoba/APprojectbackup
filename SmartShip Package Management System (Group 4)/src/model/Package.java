4089690c04ad4e92b46e95b7eca92dfe3d7f2762
Kukkinoba <elisha.beverly@gmail.com>
2025-11-07 09:55:25 -0500

    packages class

---
+++ SmartShip Package Management System (Group 4)/src/model/Package.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Package {
+	//-----------------------Main Attributes-----------------------
+	private float weight;
+	private String dimensions;
+	private int packageStatus;
+	private int packageType;
+	private int packageId;
+	
+	
+	//---------------------Default Constructor---------------------
+	
+}
