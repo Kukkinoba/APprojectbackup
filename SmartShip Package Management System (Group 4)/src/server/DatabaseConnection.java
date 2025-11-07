c9474f3520675e22eae38058a180ea6619db7646
Kukkinoba <elisha.beverly@gmail.com>
2025-11-07 10:32:23 -0500

    adding database jdbc

---
+++ SmartShip Package Management System (Group 4)/src/server/DatabaseConnection.java
@@ -0,0 +1,18 @@
+package server;
+
+public class DatabaseConnection {
+	//-----------------------Main Attributes-----------------------
+	String url = "";
+	Connection mainConn = null;
+	
+	//Overall connection code from the lab
+	public static mainConn getconnection() {
+		try {
+				mainConn =
+			
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+}
